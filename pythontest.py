b92cca136b2fc547f89720831762c948bc9c23a7
David Edwards <david.edwards@gmail.com>
2021-07-03 20:19:13 -0600

    tentatively working, missing one chunk

--- pythontest.py
+++ pythontest.py
@@ -1,46 +1,124 @@
 import threading
 import time
-from queue import Queue, PriorityQueue
-
+from queue import Queue, PriorityQueue, Empty
+import logging
 
 # class WorkPiece:
 #     def __init__(self, chunk_num, values) -> None:
 #         self.chunk_num = chunk_num
 #         self.values = values
+logging.getLogger(__name__)
+logging.basicConfig(filename="adder.log", filemode='w',
+                    encoding="utf-8", format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
+
+class ProcessFilesThread(threading.Thread):
+    def __init__(self, file1, file2, out_queue) -> None:
+        super().__init__()
+        self.file1 = file1
+        self.file2 = file2
+        self.out_queue = out_queue
+
+    def run(self):
+        with open(self.file1, 'r') as file_1, open(self.file2, 'r') as file_2:
+            chunk = []
+            chunksize = 1000
+            chunk_num = 0
+            position = 0
+            for x, y in zip(file_1, file_2):
+                if position < chunksize:
+                    chunk.append((int(x), int(y)))
+                    position += 1
+                else:
+                    logging.info("End Chunk {}".format(chunk_num))
+                    self.out_queue.put((chunk_num, chunk))
+                    chunk = []
+                    chunk_num += 1
+                    position = 0
+            self.out_queue.put((chunk_num, chunk))
+            # print("chunk {}, value {}".format(chunk_num, chunk[-1]))
+
+
+# def ProcessFiles(file1, file2):
+#     with open(file1, 'r') as file_1, open(file2, 'r') as file_2:
+#         chunk = []
+#         chunksize = 1000
+#         chunk_num = 0
+#         position = 0
+#         out_queue = PriorityQueue()
+#         for x, y in zip(file_1, file_2):
+#             if position < chunksize:
+#                 chunk.append((int(x), int(y)))
+#                 position += 1
+#             else:
+#                 logging.info("End Chunk {}".format(chunk_num))
+#                 out_queue.put((chunk_num, chunk))
+#                 chunk = []
+#                 chunk_num += 1
+#                 position = 0
+#     return out_queue
 
 
 class ProcessThread(threading.Thread):
-    def __init__(self, results) -> None:
+    def __init__(self, in_queue, out_queue) -> None:
         super().__init__()
-        self.in_list = results
-        print("Init ProcessThread")
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+        logging.info("Init ProcessThread")
 
     def run(self):
         while True:
             try:
-                chunk = self.in_list.get(block=True, timeout=1)
-                print('\n'.join(chunk[1]) + '\n')
-                self.in_list.task_done()
-            except Queue.Empty:
+                chunk = self.in_queue.get_nowait()
+                results = []
+                for line in chunk[1]:
+                    results.append(line[0]+line[1])
+                self.out_queue.put((chunk[0], results))
+                self.in_queue.task_done()
+            except Empty:
                 break
-            # with open('resultfile.txt', 'a') as file:
-            #     file.write('\n'.join(chunk.values) + '\n')
+
+
+class SaveChunk(threading.Thread):
+    def __init__(self, processed_queue) -> None:
+        super().__init__()
+        self.work_queue = processed_queue
+
+    def run(self):
+        with open("outfile.txt", "w") as out_file:
+            while True:
+                try:
+                    chunk = self.work_queue.get(block=True, timeout=2)
+                    out_file.writelines("%s\n" % num for num in chunk[1])
+                    print(chunk[0])
+                    self.work_queue.task_done()
+                except Empty:
+                    break
 
 
 if __name__ == "__main__":
-    current_chunk = 0
-    chunk1 = (1, ['2', '4', '5', '6'])
-    chunk2 = (0, ['2', '4', '2', '1'])
 
-    results = Queue()
-    results.put(chunk1)
-    results.put(chunk2)
+    file_queue = PriorityQueue()
+
+    t = ProcessFilesThread("/Users/david/Google Drive/Personal/Education/CSU-Global/CSC507 - Foundations of Operating Systems/Module 8/CSC507_PortfolioProject/hugefile1.txt",
+                           "/Users/david/Google Drive/Personal/Education/CSU-Global/CSC507 - Foundations of Operating Systems/Module 8/CSC507_PortfolioProject/hugefile2.txt", file_queue)
+    t.setDaemon(True)
+    t.start()
+    t.join()
+    print(file_queue.qsize())
+    results_queue = PriorityQueue()
 
     threads = []
-    for i in range(0, 2):
-        t = ProcessThread(results)
+    for _ in range(8):
+        t = ProcessThread(file_queue, results_queue)
         threads.append(t)
         t.setDaemon(True)
         t.start()
+        t.join()
 
-    results.join()
+    print("Out of process")
+    save_thread = SaveChunk(results_queue)
+    save_thread.setDaemon(True)
+    save_thread.start()
+
+    save_thread.join()
